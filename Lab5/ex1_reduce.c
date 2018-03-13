b53fa4ad3738e0fc894bf1b50e799033a6e7623f
Lê Quang Trình <1513687@hcmut.edu.vn>
2018-03-13 15:46:03 +0700

    ex1 Lab5

---
+++ Lab5/ex1_reduce.c
@@ -0,0 +1,23 @@
+#include <mpi.h>
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+    int numtasks, rank, size ,dest, source, rc, count, tag = 1;
+    
+    MPI_Status Stat; // required variable for receive routines
+    int local, global;
+
+    
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    local = rank+1;
+    MPI_Reduce(&local,&global,1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);    
+
+    if(rank ==0)
+        print("Sum is : %d", global)
+    MPI_Finalize();
+    return 0;
+}
