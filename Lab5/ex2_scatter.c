8a7062b845f403f2f73d565b95c54582f48f264d
HappyTree97 <trinhle9716@gmail.com>
2018-03-14 03:47:38 +0000

    "ex 2"

--- Lab5/ex2_scatter.c
+++ Lab5/ex2_scatter.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    printf("Number of processes is %d",size)
+    printf("Number of processes is %d",size);
     // sendRootCount = n / size;
     if (rank = 0)
     {
