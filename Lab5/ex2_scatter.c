9272bbe844f59bdfbba96175fe6c586a5ef728a8
Lê Quang Trình <1513687@hcmut.edu.vn>
2018-03-14 10:28:51 +0700

    ex2

--- Lab5/ex2_scatter.c
+++ Lab5/ex2_scatter.c
@@ -2,66 +2,67 @@
 #include <stdio.h>
 int main(int argc, char **argv)
 {
-    int numtasks, rank, size ,dest, source, rc, count, tag = 1;
-    
+    int numtasks, rank, size, dest, source, rc, count, tag = 1;
+
     MPI_Status Stat; // required variable for receive routines
     int n = 100;
     int a;
-    int * sendRoot ;
-    int * recvRoot;
+    int *sendRoot;
+    int *recvRoot;
     int sendClient;
-    int * recvClient;
-    int i=0;
-    int sendRootCount=0, recvRootCount=0;
-    int sendClientCount =0, recvClientCount =0;
-    
+    int *recvClient;
+    int i = 0;
+    int sendRootCount = 0, recvRootCount = 0;
+    int sendClientCount = 0, recvClientCount = 0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    sendRootCount=n/size;
-    if(rank =0){
-<<<<<<< HEAD
-        sendRootCount= recvRootCount =n/size;
-=======
-        
->>>>>>> 340980cafd25718202998b02127ed60566bc8cc7
-        
-        if(n%size==0){
-            recvRootCount= size;
-            sendRoot = malloc(n*sizeof(int));
-            for( i=0; i<n;i++){
+    printf("Number of processes is %d",size)
+    // sendRootCount = n / size;
+    if (rank = 0)
+    {
+        sendRootCount = recvRootCount = n / size;
+
+        if (n % size == 0)
+        {
+            sendRoot = malloc(n * sizeof(int));
+            for (i = 0; i < n; i++)
+            {
                 sendRoot[i] = i;
             }
         }
-        else{
-            recvRootCount= size+1;
-            int t =(size+1)*sendRootCount;
-            sendRoot = malloc(t*sizeof(int));
-            for(i=0; i<t; i++){
-                if(i<n)
-                    sendRoot[i]=i;
-                else 
-                    sendRoot[i] =0;
+        else
+        {
+            int t = (size + 1) * sendRootCount;
+            sendRoot = malloc(t * sizeof(int));
+            for (i = 0; i < t; i++)
+            {
+                if (i < n)
+                    sendRoot[i] = i;
+                else
+                    sendRoot[i] = 0;
             }
         }
     }
-    sendClientCount = 1;
-    recvClientCount = n/size;
-    MPI_Scatter(&sendRoot, sendRootCount, MPI_INT, &recvRoot, sendRootCount, MPI_INT, 0, MPI_COMM_WORLD);
-    
-    
-    int local =0;
-    for(i =0; i<recvRootCount; i++){
+
+    MPI_Scatter(&sendRoot, sendRootCount, MPI_INT, &recvRoot, recvRootCount, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int local = 0;
+    for (i = 0; i < recvRootCount; i++)
+    {
         local += recvRoot[i];
-    } 
-    printf(
+    }
+
+    printf("Local sum is %d ", local);
 
     MPI_Gather(&local, 1, MPI_INT, &recvClient, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    if(rank ==0){
-        int global =0;
-	printf(sendRootCount);
-        for(i=0;i<sendRootCount;i++){
+    if (rank == 0)
+    {
+        int global = 0;
+        printf(sendRootCount);
+        for (i = 0; i < sendRootCount; i++)
+        {
             global += recvClient[i];
         }
         printf("Global sum is %d", global);
