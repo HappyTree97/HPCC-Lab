f18c6e740d5944182043bf7a44aaf8e6feff87bc
Lê Quang Trình <1513687@hcmut.edu.vn>
2018-03-14 10:10:17 +0700

    req merge code

--- Lab5/ex2_scatter.c
+++ Lab5/ex2_scatter.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     if(rank =0){
-        sendRootCount=n/size;
+        sendRootCount= recvRootCount =n/size;
         
         if(n%size==0){
             recvRootCount= size;
