1768a12654651ca55cfb3cb1a3ec0a69a660396a
Lê Quang Trình <1513687@hcmut.edu.vn>
2018-03-23 20:00:43 +0700

    Lab 3

--- Lab4/lab4_1.c
+++ Lab4/lab4_1.c
@@ -2,22 +2,21 @@
 #include <stdio.h>
 #include <math.h>
 
-int isprime(int n) {
-int i,squareroot;
-if (n>10) {
-   squareroot = (int) sqrt((double) n);
-   for (i=3; i<=squareroot; i=i+2)
-      if ((n%i)==0)
-         return 0;
-   return 1;
-   }
-else
-	if (n == 2 || n == 3 || n == 5 || n ==7 ) return 1;
-    else
-    	return 0;
+bool isprime(int n)
+{
+	int i;
+	if (n >= 2)
+	{
+		for (i = 2; i*i <= n; i = i + 2)
+			if ((n % i) == 0)
+				return false;
+		return true;
+	}
+	else 
+		return false;
 }
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int rank;
 	int nTasks;
@@ -25,32 +24,28 @@ int main (int argc, char *argv[])
 	long limit = 1000000000;
 	int source = 0;
 
-	MPI_Init(&argc,&argv);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Comm_size(MPI_COMM_WORLD,&nTasks);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nTasks);
 
-	long step = nTasks*2;
-	long start = rank*2 + 1;
+	long step = nTasks * 2;
+	long start = rank * 2 + 1;
 	long i;
 	pCount = 0;
-	double start_time = MPI_Wtime(); 
-	if (rank == source){
-		for (i = start; i <= limit; i+=step){
-			if (isprime(i) == 1) {
-				pCount++;
-			}
-		}
-		MPI_Reduce(&pCount,&pCountSum,1,MPI_INT,MPI_SUM,source,MPI_COMM_WORLD);
-		double end_time=MPI_Wtime();
+	double start_time = MPI_Wtime();
+	for (i = start; i <= limit; i += step)
+		if (isprime(i))
+			pCount++;
+	if (rank == source)
+	{
+		MPI_Reduce(&pCount, &pCountSum, 1, MPI_INT, MPI_SUM, source, MPI_COMM_WORLD);
+		double end_time = MPI_Wtime();
 		printf("So prime number tu 1 -> 1000000000 la: %d\n", pCountSum + 1); // bo qua so chan nen chua tinh 2.
-		printf("Mat thoi gian: %.2lf\n", end_time - start_time); 
-	} else {
-		for (i = start; i <= limit; i+=step){
-			if (isprime(i) == 1) {
-				pCount++;
-			}
-		}
-		MPI_Reduce(&pCount,&pCountSum,1,MPI_INT,MPI_SUM,source,MPI_COMM_WORLD);
+		printf("Mat thoi gian: %.2lf\n", end_time - start_time);
+	}
+	else
+	{
+		MPI_Reduce(&pCount, &pCountSum, 1, MPI_INT, MPI_SUM, source, MPI_COMM_WORLD);
 	}
 	MPI_Finalize();
 }
--- Lab4/lab4_2.c
+++ Lab4/lab4_2.c
@@ -1,75 +1,58 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <math.h>
-int main (int argc, char *argv[])
+
+#define _128KB 16384
+#define _256KB 32768
+#define _512KB 65536
+#define _1024KB 131072
+int main(int argc, char *argv[])
 {
+
 	int rank;
 	int target;
 	int nTasks;
-	double sendArr[16384], recvArr[16384];
-	MPI_Init(&argc,&argv);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Comm_size(MPI_COMM_WORLD,&nTasks);
+	int numTest = 5;
+	double sendArr[_128KB], recvArr[_128KB];
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nTasks);
 	MPI_Status Stat;
 	int i;
-	for (i = 0; i < 16384; i++) sendArr[i] = 1.2;
-	if (rank == 0)  target = nTasks - 1;
-	if (rank == nTasks - 1) target = 0;
-	double 	start_time,
-			end_time,
-			avg_time;
+	for (i = 0; i < _128KB; i++)
+		sendArr[i] = 1.2;
+	if (rank == 0)
+		target = nTasks - 1;
+	if (rank == nTasks - 1)
+		target = 0;
+	double start_time,
+		end_time,
+		avg_time;
 	avg_time = 0;
-	if (rank == 0) {
-		start_time = MPI_Wtime(); 
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
-		end_time = MPI_Wtime();
-		avg_time = end_time - start_time + avg_time;
-
-		start_time = MPI_Wtime(); 
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
-		end_time = MPI_Wtime();
-		avg_time = end_time - start_time + avg_time;
-
-		start_time = MPI_Wtime(); 
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
-		end_time = MPI_Wtime();
-		avg_time = end_time - start_time + avg_time;
-
-		start_time = MPI_Wtime(); 
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
-		end_time = MPI_Wtime();
-		avg_time = end_time - start_time + avg_time;
-
-		start_time = MPI_Wtime(); 
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
-		end_time = MPI_Wtime();
-		avg_time = end_time - start_time + avg_time;
+	if (rank == 0)
+	{
+		for (i = 0; i < numTest; i++)
+		{
+			start_time = MPI_Wtime();
+			MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
+			MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
+			end_time = MPI_Wtime();
+			avg_time = end_time - start_time + avg_time;
+		}
 
 		avg_time = avg_time / 5;
 
 		printf("Time: %f\n", avg_time);
-		printf("BandWidth: %f kbit/s\n", 128*8 / avg_time);
+		printf("BandWidth: %f kbit/s\n", 128 * 8 / avg_time);
 	}
-	else if (rank == nTasks - 1){
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
+	else if (rank == nTasks - 1)
+	{
 
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
-
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
-
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
-
-		MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
-		MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
+		for (i = 0; i < numTest; i++)
+		{
+			MPI_Send(sendArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD);
+			MPI_Recv(recvArr, 1, MPI_DOUBLE, target, 1, MPI_COMM_WORLD, &Stat);
+		}
 	}
 	MPI_Finalize();
 }
