fe858764cbe1b403c05a43337c851d4a4398554b
Lê Quang Trình <1513687@hcmut.edu.vn>
2018-03-26 15:23:16 +0700

    aaaaa

--- Lab3/matrix2.cc
+++ Lab3/matrix2.cc
@@ -2,58 +2,50 @@
 #include <cstdlib>
 #include <omp.h>
 
-int main()
-{
-    const int n = 3000; // This problem scales as n^3.
-                        // This value may need to be adjusted
+int main(){
+  	const int n=3000; // This problem scales as n^3. 
+                    // This value may need to be adjusted
 
-    double *A = (double *)malloc(sizeof(double) * n * n);
-    double *B = (double *)malloc(sizeof(double) * n * n);
-    double *C = (double *)malloc(sizeof(double) * n * n);
+  	double * A = (double*) malloc(sizeof(double)*n*n);
+  	double * B = (double*) malloc(sizeof(double)*n*n);
+  	double * C = (double*) malloc(sizeof(double)*n*n);
+  
+  	printf("Carrying out matrix-matrix multiplication\n");
 
-    printf("Carrying out matrix-matrix multiplication\n");
+ 
+ 	int i,j,k;
 
-    // Cilk Plus array notation
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            A[i * n + j] = (double)i / (double)n;
-            B[i * n + j] = (double)j / (double)n;
-        }
-    }
+	for (i = 0 ; i < n; i++) {
+		for(j = 0; j < n; j++) {
+	      A[i*n+j]=(double)i/(double)n;
+	      B[i*n+j]=(double)j/(double)n;
+	    }
+	}
 
-    C [0:n * n] = 0.0;
+	C[0:n*n] = 0.0;
 
-    // C = A x B
-     
-    double time = omp_get_wtime();
-    #pragma omp parallel for  
-    //#pragma omp simd
-    for (int i = 0; i < n; i++)
-    {
-	#pragma omp simd 
-        for (int j = 0; j < n; j++)
-        {
-	    //#pragma omp simd
-            for (int k = 0; k < n; k++)
-            {
-                C[i * n + j] += A[i * n + k] * B[k * n + j];
-            }
-        }
-    }
-    time = omp_get_wtime() - time;
+	double time = omp_get_wtime();
+	
+	for ( i = 0 ; i < n ; i++){
+	    for ( j = 0 ; j < n ; j++) {
+	    	#pragma omp simd
+	      	for ( k = 0 ; k < n ; k++) {
+				C[i*n+k] += A[i*n+j]*B[j*n+k];
+	      	}
+	    }
+	}
+	  time = omp_get_wtime() - time;
+	  
+	  printf("Checking the results...\n");
+	  double norm = 0.0;
+	  for ( i = 0 ; i < n ; i++)
+	    for ( j = 0 ; j < n ; j++)
+	      norm += (C[i*n+j]-(double)(i*j)/(double)n)*(C[i*n+j]-(double)(i*j)/(double)n);
+	  
+	  if (norm > 1e-10)
+	    printf("Something is wrong... Norm is equal to %f\n", norm);
+	  else
+	    printf("Yup, we're good!\n");
 
-    printf("Checking the results...\n");
-    double norm = 0.0;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            norm += (C[i * n + j] - (double)(i * j) / (double)n) * (C[i * n + j] - (double)(i * j) / (double)n);
-
-    if (norm > 1e-10)
-        printf("Something is wrong... Norm is equal to %f\n", norm);
-    else
-        printf("Yup, we're good!\n");
-
-    printf("Computing time: %f\n", time);
+	  printf("Computing time: %f\n", time);  
 }
