092f256b7ebf6fbb8d9de36bcd9ad61892106a15
Lê Quang Trình <1513687@hcmut.edu.vn>
2018-05-30 23:25:51 +0700

    up file

--- assignment/apriori_quoc_ss.cpp
+++ assignment/apriori_quoc_ss.cpp
@@ -195,12 +195,14 @@ largeItemSet Apriori::generateCandidates(largeItemSet &preL){
                         cout<< itemset_vec[t] << " ";
                     }
                 }
+                cout<<endl;
                 insert(itemset_vec, item);
                 if(i==j==2){
                     for(int t =0; t< itemset_vec.size(); t++){
                         cout<< itemset_vec[t] << " ";
                     }
                 }
+                cout<<endl;
                 to_gennerate = true;
                 if(newCset.count(itemset_vec)==0){
                     temp = itemset_vec;
